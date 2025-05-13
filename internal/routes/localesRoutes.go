c506174dc2de648ff3e962a74062572d7830b9c8
ariel-madrid <ariel.argomedo@usach.cl>
2025-05-13 13:08:19 -0400

    routas fixed

--- internal/routes/localesRoutes.go
+++ internal/routes/localesRoutes.go
@@ -2,14 +2,13 @@ package routes
 
 import (
 	"github.com/ariel-madrid/agent-service-go/controller"
-	"github.com/ariel-madrid/agent-service-go/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 // Estas son las rutas del backend - Los endpoints a los que el Frontend va a consultar po informacion
 func InitLocalesRoutes(r *gin.Engine) {
-	AlumnoGroup := r.Group("/api/v1/buscador-estudiantes")
+	LocalesGroup := r.Group("/api/v1")
 
-	AlumnoGroup.GET("/locales", controller.GetPlanesAlumno)
+	LocalesGroup.GET("/locales", controller.GetLocales)
 	
 }
